31e8bf85b9cedda9618b9585ffda537d8a41f9fe
RichardVargas1 <richard.vargas95@gmail.com>
2018-12-20 23:04:23 -0600

    Commented-out code not needed yet

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-const axios = require("axios");
-const cheerio = require("cheerio");
+// const axios = require("axios");
+// const cheerio = require("cheerio");
 
 // Require all models
-const db = require("./models");
+//const db = require("./models");
 
 const PORT = 3000;
 
@@ -19,15 +19,17 @@ const app = express();
 // Configure middleware:
 
 // Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
+    // app.use(logger("dev"));
+    // Parse request body as JSON
+    // app.use(express.urlencoded({ extended: true }));
+    // app.use(express.json());
+    // Make public a static folder
+    // app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// Mongo deployed database
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // Start the server
 app.listen(PORT, function () {
